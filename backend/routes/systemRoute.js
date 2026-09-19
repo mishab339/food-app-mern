a0ab44064eee70790c47543681c99ab6f5180baf
mishab <mishab1441@gmail.com>
2026-09-19 22:57:37 +0530

    feat: created an api for deleting files from disk

---
+++ backend/routes/systemRoute.js
@@ -0,0 +1,17 @@
+import express from "express";
+const systemRouter = express.Router();
+
+systemRouter.delete("/api/admin/clear-images", (req, res) => {
+  const uploadDir = path.join(process.cwd(), "uploads");
+
+  fs.readdirSync(uploadDir).forEach((file) => {
+    fs.unlinkSync(path.join(uploadDir, file));
+  });
+
+  res.json({
+    success: true,
+    message: "Images cleared",
+  });
+});
+
+export default systemRouter
--- backend/server.js
+++ backend/server.js
@@ -1,35 +1,36 @@
-import "dotenv/config"
-import express from "express"
-import cors from "cors"
-import {connectDB} from './config/db.js'
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import { connectDB } from "./config/db.js";
 import foodRouter from "./routes/foodRoute.js";
 import userRouter from "./routes/userRoutes.js";
 import cartRouter from "./routes/cartRoute.js";
 import orderRouter from "./routes/orderRoute.js";
-
+import systemRouter from "./routes/systemRoute.js";
 
 //app config
 const app = express();
 const port = 4000;
 
 //middleware
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 //db connection
 connectDB();
 
 //api endpoints
-app.use("/images",express.static("uploads"));
-app.use("/api/food",foodRouter);
-app.use("/api/user",userRouter);
-app.use("/api/cart",cartRouter);
-app.use("/api/order",orderRouter);
+app.use("/images", express.static("uploads"));
+app.use("/api/food", foodRouter);
+app.use("/api/user", userRouter);
+app.use("/api/cart", cartRouter);
+app.use("/api/order", orderRouter);
+app.use("/api/system", systemRouter);
 
-app.get("/",(req,res)=>{
-    res.send("Hello World")
-})
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
-app.listen(port,()=>{
-    console.log(`server is running on port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
+});
