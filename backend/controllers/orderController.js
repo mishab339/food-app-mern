5a224bb35b111f29d9a1138fedc8bec234b1c5d5
mishab <mishab1441@gmail.com>
2026-09-20 14:39:06 +0530

    fix: remvoed static url to env

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -5,7 +5,7 @@ import Stripe from "stripe";
 const strip = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 const placeOrder = async (req, res) => {
-  const frontend_url = "http://localhost:5173";
+  const frontend_url = process.env.FRONTEND_URL;
   try {
     const newOrder = new orderModel({
       userId: req.body.userId,
